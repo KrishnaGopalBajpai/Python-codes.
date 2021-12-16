207e782f3aabc372fbcc94ab790416d9be7ca3c7
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-16 22:32:57 +0530

    Create identifier.py

--- identifier.py
+++ identifier.py
@@ -4,6 +4,17 @@
 #Never Start with a number.
 #Use underscores to saprate two words
 #Keywords cannot be identifiers
+#example of identifiers.
+#Valied identifiers
+#age = 24
+#name = "Krishna"
+#last name = "Bajpai"
+#_id = '24683x65'
+#name= "Reta"
+#invalied identifiers
+#2name= "Priya" no identifiers starts with the numeric char.
+#marks$= 98 no char other then _ allowed as an identifier.
+#class = "Hello" bcoz it's key word.
 
 age = 24;
 name = "Krishna"
