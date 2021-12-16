95a29bc0c9f011e386259b0a36f80b8324b80396
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-16 22:47:15 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -1,1 +1,16 @@
+#Python Data type
+#Integer and long
+#float/floating point
+#complex Numbers
+#Boolean
+#none
+#sequence
+#sets
+#mapping
 
+rating = 4.7
+name = "Anuj"
+marks = 98
+is_passed = true 
+my_complex_number = 4+5j
+fruits_list = ['apple', 'kivi','pineapple']
