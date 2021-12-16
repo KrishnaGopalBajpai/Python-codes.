ac77f4f405d086e2aebfe3fcf3a93907905299ba
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-16 17:05:03 +0530

    Create identifier.py

---
+++ identifier.py
@@ -0,0 +1,11 @@
+#Python Identifiers:-
+#Rules for writing identifiers.
+#use only alphanumeric characters and underscores.
+#Never Start with a number.
+#Use underscores to saprate two words
+#Keywords cannot be identifiers
+
+age = 24;
+name = "Krishna"
+print (age)
+print (name)
