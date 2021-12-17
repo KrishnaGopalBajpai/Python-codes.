379ae460c188dd7d3ca6ce7cc99c61688dcd08d4
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-17 13:32:44 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -14,17 +14,28 @@ name = "Anuj"
 print (name,type (name))
 marks = 98
 print (marks,type (marks))
-is_passed = true
+is_passed = True
 print (is_passed,type (is_passed))
-my_complex_number = 4+5j
+my_complex_number= 4+5j
 print (my_complex_number,type (my_complex_number))
 fruits_list = ['apple', 'kivi','pineapple']
 print(fruits_list, type (fruits_list))
 
 
-Q write a program to save following values of a user.
-name
-age 
-year of birth
-is new user?
-listof his/her favorite food?
+#Q write a program to save following values of a user.
+#name
+#age 
+#year of birth
+#is new user?
+#listof his/her favorite food?
+#solution:- 
+name = "Krishna"
+print (name, type(name))
+age = 24
+print (age, type(age))
+year_of_birth = 1997 
+print (year_of_birth, type(year_of_birth))
+is_new_user = True
+print (is_new_user, type (is_new_user))
+favorite_food = ['Kivi', 'Pizza']
+print (favorite_food, type (favorite_food))
