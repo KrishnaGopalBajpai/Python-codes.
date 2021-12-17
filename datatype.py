a7ad79edf4f6625cc89b310b2241a32ed26d2ebb
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-17 13:16:08 +0530

    Update datatype.py

--- datatype.py
+++ datatype.py
@@ -9,8 +9,22 @@
 #mapping
 
 rating = 4.7
+print (rating,type (rating))
 name = "Anuj"
+print (name,type (name))
 marks = 98
-is_passed = true 
+print (marks,type (marks))
+is_passed = true
+print (is_passed,type (is_passed))
 my_complex_number = 4+5j
+print (my_complex_number,type (my_complex_number))
 fruits_list = ['apple', 'kivi','pineapple']
+print(fruits_list, type (fruits_list))
+
+
+Q write a program to save following values of a user.
+name
+age 
+year of birth
+is new user?
+listof his/her favorite food?
