6b262e866a604ecce9a50b2206dfd214450d43b4
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-18 00:16:22 +0530

    Update loops(while).py

--- loops(while).py
+++ loops(while).py
@@ -2,3 +2,36 @@
 #we generally use this loop when we don't know beforehand, the number of times to iterate.
 # while test_expresion
 #    body of while
+
+#ex-1)
+n = 5
+while n>=0:
+    print(n)
+    n -=1
+
+  '''
+output:-
+5
+4
+3
+2
+1
+0
+  '''
+ #ex-2)
+n = 5
+while n>=0:
+    print(n)
+    
+ '''
+ output:- infinite loop.
+ '''
+#ex-3
+n = 5
+while n>=0:
+    print(n, end=",")
+    n -=1
+'''
+output:- 
+5,4,3,2,1,0,
+'''
