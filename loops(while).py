6af1a6c035b4c860c03c5383d2f314078e1aa3b9
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-17 23:40:24 +0530

    Update loops(while).py

--- loops(while).py
+++ loops(while).py
@@ -1,1 +1,4 @@
-
+#The while loops in python is used to iterate over a block of code as long as the test expression(condition) is true.
+#we generally use this loop when we don't know beforehand, the number of times to iterate.
+# while test_expresion
+#    body of while
