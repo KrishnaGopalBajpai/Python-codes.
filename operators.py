d435c4c03fd721da747f635d4d4241f3f4aec0f6
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-17 15:34:36 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,122 @@
+#Arithmetic Operators ( +,-,*,/,%,//,**)
+a=5
+b=9
+c=a+b
+print (c)
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a%b)
+print (a//b)
+print (a**b)
+
+''' 
+Console output for print:- 
+24
+24
+6
+135
+1.6666666666666667
+6
+1
+38443359375
+'''
+
+#Assignment Operators (=,+=,-=,*=,/=,%=,//=,**=,&=,|=,^=,>>=,<<=)
+a=10
+print('=', a)
+a +=5
+print ('+=', a)
+a -= 5
+print ('-=', a)
+a *=2
+print ('*=', a)
+a /= 3
+print ('/=',a)
+a %= 3
+print ('%=", a)
+a //= 4
+print ('//=',a)
+a **= 2
+print ('**=',a)
+'''
+a &= 3
+print ('&=',a)
+a |= 5
+print ('|=', a)
+a ^= 5
+print ('^=',a)
+a >>= 8
+print ('>>=', a)
+a <<= 9
+print ('<<=', a)
+'''
+
+console output :- 
+'''= 10
++= 15
+-= 10
+*= 20
+/= 6.666666666666667
+%= 0.666666666666667
+//= 0.0
+**= 0.0
+'''
+#Relationl/Comparison Operators (==,!=, >, <, >=, <=)
+a=40
+b= 23
+print ('a>b', a>b)
+print ('a<b', a<b)
+print ('a >= b',a>=b)
+print ('a<=b', a<=b)
+print ('a==b', a==b)
+print ('a!=b', a!=b)
+
+'''
+console output:- 
+  a>b True
+  a<b False
+  a >= b True
+  a<=b False
+  a==b False
+  a!=b True '''
+
+#Logical Operators (and, or, not)
+x = True
+y = False
+
+print ('x and y is', x and y)
+print ('x or y is', x or y)
+print ('not y is', not y)
+print ('not x is', not x)
+
+'''
+console output:- 
+x and y is False
+x or y is True
+not y is True
+not x is False
+'''
+
+
+#Identitiy Operators (is ,is_not)
+#Membership Operators (in, notin)
+#Bitwise Operators (&, |, ^, ~, <<, >>)
+
+Q) write a program to take the average of three numbers?
+
+first_number = 10
+second_number = 20
+third_number = 30
+average = (first_number+second_number+third_number)/3
+print((first_number+second_number+third_number)/3)
+print(average)
+
+'''first_number = 10
+second_number = 20
+third_number = 30
+average = (first_number
++second_number+
+third_number)/3
+print (average)'''
