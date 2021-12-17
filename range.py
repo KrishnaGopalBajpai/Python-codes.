13eedce4014eac5dfde4a96de82f03909a6957dd
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-17 17:01:28 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,6 +1,6 @@
 #we can generate a sequence of numbers using range() function on the go not stored them in.
 #suppose range(10) will generate number from 0 to 9 (10 nos). -> [0,1,2,3,4,5,6,7,8,9]
-#syntex : range (start,stop,stepsize). defaults value of stepsize is one if not given.
+#syntex : range ((inclusive)start,(exclusive)stop,stepsize). defaults value of stepsize is one if not given.
 
 #ex1)
 
@@ -10,4 +10,31 @@ print (a)
 
 '''
 output:- range(0, 10)
+'''
+#ex2) list the number from 0 to 9.
+a = list (range (10))
+
+print (a)
+
+'''
+output:- [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+'''
+
+
+#ex3) list the number from 5 to 9.
+a = list (range (5,10))
+
+print (a)
+
+'''
+output:- [5, 6, 7, 8, 9]
+'''
+
+#ex2) list the number from 0 to 10 with stepsize2.
+a = list (range (0,11,2))
+
+print (a)
+
+'''
+output:- [0, 2, 4, 6, 8, 10]
 '''
