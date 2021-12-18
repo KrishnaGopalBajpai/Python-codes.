cff1ba4c1bafc1050d74a4972a338edcae92dd33
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-18 22:57:44 +0530

    Update lists(comprehension).py

--- lists(comprehension).py
+++ lists(comprehension).py
@@ -1,1 +1,7 @@
+#List comprehension is an elegant and concise way to create a new list from an existing list in python.
+#list comprehension consists of an expression followed by for statement inside square brackets.
+# new_list =[expressio foriteminlist ifcondition]
+#1. Pow2 = [2 ** x forx in range(10)]
+#  output: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+# print(Pow2)
 
