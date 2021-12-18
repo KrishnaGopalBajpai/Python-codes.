c779c193fb1565127d62faa9b54e8db5d7176b2f
Krishna Gopal Bajpai <37985929+KrishnaGopalBajpai@users.noreply.github.com>
2021-12-18 22:07:09 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,8 @@
+#Python lists:-
+#In python programming a list is created by placing all the items(elements) inside a square bracket[],
+#separated by commas.
+#it can have any number of items and they may be of different types (integer, float, string etc.)
+#1. my_list = []
+#2. my_list = [ 1, 2, 3]
+#3. my_list = [1, 'Hellow', 3.4]
+
